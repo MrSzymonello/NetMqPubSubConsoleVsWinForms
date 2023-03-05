758823984d2e8e9b17983acddb2f4a800e2e4561
Szymon Szyszko <szymon.szyszko@schange.com>
2023-03-05 21:24:40 +0100

    Fix Windows Forms publisher

--- src/PublisherWindowsFormsApp/Form1.cs
+++ src/PublisherWindowsFormsApp/Form1.cs
@@ -7,21 +7,18 @@ namespace PublisherWindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        private readonly PublisherSocket publisher = new PublisherSocket();
         public Form1()
         {
+            publisher.Bind("tcp://127.0.0.1:8888");
             InitializeComponent();
         }
 
         private void buttonPublish_Click(object sender, EventArgs e)
         {
-            using (var publisher = new PublisherSocket())
-            {
-                publisher.Bind("tcp://127.0.0.1:8888");
-
-                publisher
+            publisher
                     .SendMoreFrame("COMMAND")
                     .SendFrame(textBoxCommand.Text);
-            }
         }
     }
 }
